a0d0c2f23ad4b3177f4ab17ea329299bc0f4aae9
Savraj <savrajcheema@hotmail.com>
2023-01-02 03:33:02 -0800

    style: prettier formatted and removed comment

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
-'use strict';
-const { json } = require('body-parser');
-const fetch =  require('cross-fetch');
-const Twitter = require('twitter');
+"use strict";
+const { json } = require("body-parser");
+const fetch = require("cross-fetch");
+const Twitter = require("twitter");
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -12,7 +12,9 @@ const client = new Twitter({
 
 module.exports.twitBot = async () => {
   try {
-    const res = await fetch('https://geek-jokes.sameerkumar.website/api?format=json');
+    const res = await fetch(
+      "https://geek-jokes.sameerkumar.website/api?format=json"
+    );
 
     if (res.status >= 400) {
       throw new Error("Bad response from server");
@@ -20,19 +22,17 @@ module.exports.twitBot = async () => {
 
     const jsonjoke = await res.json();
     const joke = JSON.stringify(jsonjoke.joke);
-    console.log(joke)
-      try{
-        const twit = await client.post('statuses/update', {status: joke});
-        if (twit.status >= 400){
-          throw new Error('Bad response from server')
-        }
-        console.log(twit)
-      }catch(err){
-        console.log(err);
+    console.log(joke);
+    try {
+      const twit = await client.post("statuses/update", { status: joke });
+      if (twit.status >= 400) {
+        throw new Error("Bad response from server");
       }
-    }catch (err) {
+      console.log(twit);
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
     console.error(err);
   }
-  
-  
 };
